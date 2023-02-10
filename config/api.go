cb5e0996616d5d3ff3f7c3c523c98be5337af8c1
lizongti <lzongti@gmail.com>
2023-02-10 16:32:35 +0800

    fix `config.JSON`

--- config/api.go
+++ config/api.go
@@ -14,6 +14,13 @@ func Assert(condition bool, v interface{}, args ...string) {
 }
 
 func JSON(args ...string) string {
+	if len(args) == 0 {
+		data, err := json.Marshal(c.AllSettings())
+		if err != nil {
+			panic(err)
+		}
+		return string(data)
+	}
 	s := strings.Join(args, ".")
 	data, err := json.Marshal(c.Sub(s).AllSettings())
 	if err != nil {
