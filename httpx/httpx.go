b0af1d1f473579ede43c3ca353e92c602abf56dc
lizongti <lzongti@gmail.com>
2023-09-26 15:12:21 +0800

    fix httpx

--- httpx/httpx.go
+++ httpx/httpx.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"time"
@@ -12,9 +13,21 @@ import (
 type DoFunc func(*http.Client) (*http.Response, error)
 
 type ClientConfig struct {
-	Proxy   string
 	Retry   int64
 	Timeout time.Duration
+
+	// Transport
+	Proxy                 string
+	DialerTimeout         time.Duration
+	DialerKeepAlive       time.Duration
+	ForceAttemptHTTP2     bool
+	MaxIdleConns          int
+	MaxIdleConnsPerHost   int
+	MaxConnsPerHost       int
+	IdleConnTimeout       time.Duration
+	TLSHandshakeTimeout   time.Duration
+	ResponseHeaderTimeout time.Duration
+	ExpectContinueTimeout time.Duration
 }
 
 type Client struct {
@@ -23,9 +36,21 @@ type Client struct {
 }
 
 var DefaultClientConfig = ClientConfig{
-	Proxy:   "",
 	Retry:   1,
-	Timeout: 0,
+	Timeout: 60 * time.Second,
+
+	// Transport
+	Proxy:                 "",
+	DialerTimeout:         30 * time.Second,
+	DialerKeepAlive:       30 * time.Second,
+	ForceAttemptHTTP2:     true,
+	MaxIdleConns:          0,
+	MaxIdleConnsPerHost:   2,
+	MaxConnsPerHost:       0,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ResponseHeaderTimeout: 10 * time.Second,
+	ExpectContinueTimeout: 1 * time.Second,
+	IdleConnTimeout:       180 * time.Second,
 }
 
 func Default() *Client {
@@ -35,7 +60,23 @@ func Default() *Client {
 func NewClient(config ClientConfig) *Client {
 	c := &Client{
 		Config: config,
-		Client: &http.Client{},
+		Client: &http.Client{
+			Transport: &http.Transport{
+				Proxy: nil,
+				DialContext: (&net.Dialer{
+					Timeout:   config.DialerTimeout,
+					KeepAlive: config.DialerKeepAlive,
+				}).DialContext,
+				ForceAttemptHTTP2:     config.ForceAttemptHTTP2,
+				MaxIdleConns:          config.MaxIdleConns,
+				MaxIdleConnsPerHost:   config.MaxIdleConnsPerHost,
+				MaxConnsPerHost:       config.MaxConnsPerHost,
+				TLSHandshakeTimeout:   config.TLSHandshakeTimeout,
+				ResponseHeaderTimeout: config.ResponseHeaderTimeout,
+				ExpectContinueTimeout: config.ExpectContinueTimeout,
+				IdleConnTimeout:       config.IdleConnTimeout,
+			},
+		},
 	}
 
 	if c.Config.Proxy != "" {
@@ -44,9 +85,7 @@ func NewClient(config ClientConfig) *Client {
 			log.Panicln(err)
 		}
 
-		c.Client.Transport = &http.Transport{
-			Proxy: http.ProxyURL(proxyURL),
-		}
+		c.Client.Transport.(*http.Transport).Proxy = http.ProxyURL(proxyURL)
 	}
 
 	c.Client.Timeout = c.Config.Timeout
