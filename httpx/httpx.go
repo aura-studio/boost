e724b65aaee3091871007fa996c365eee3721ec4
lizongti <lzongti@gmail.com>
2024-01-11 20:03:45 +0800

    fix httpx.go

--- httpx/httpx.go
+++ httpx/httpx.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
 	"time"
 )
@@ -33,6 +34,17 @@ type ClientConfig struct {
 type Client struct {
 	Config ClientConfig
 	*http.Client
+}
+
+type DumpData struct {
+	Request               string
+	DumpRequestError      string
+	RequestBody           string
+	DumpRequestBodyError  string
+	Response              string
+	DumpResponseError     string
+	ResponseBody          string
+	DumpResponseBodyError string
 }
 
 var DefaultClientConfig = ClientConfig{
@@ -98,31 +110,127 @@ func Request(doFunc DoFunc) ([]byte, error) {
 }
 
 func (c *Client) Request(doFunc DoFunc) (data []byte, err error) {
-	var request = func() (data []byte, err error) {
-		resp, err := doFunc(c.Client)
-		defer func() {
-			if resp != nil {
-				resp.Body.Close()
-			}
-		}()
-		if err != nil {
-			return nil, err
-		}
-		if resp.StatusCode != 200 {
-			return nil, fmt.Errorf("status code of http response is %d instead of 200", resp.StatusCode)
-		}
-		return io.ReadAll(resp.Body)
-	}
-
 	var times int64
 	for times < c.Config.Retry {
 		times++
-		data, err = request()
-		if err != nil {
-			continue
+		data, err = c.do(doFunc)
+		if err == nil {
+			return
 		}
-		return data, nil
 	}
 
-	return nil, err
+	return
+}
+
+func RequestWithDump(doFunc DoFunc) ([]byte, *DumpData, error) {
+	return Default().RequestWithDump(doFunc)
+}
+
+func (c *Client) RequestWithDump(doFunc DoFunc) (data []byte, dumpData *DumpData, err error) {
+	var times int64
+	for times < c.Config.Retry {
+		times++
+		data, dumpData, err = c.doDump(doFunc)
+		if err == nil {
+			return
+		}
+	}
+
+	return
+}
+
+func (c *Client) do(doFunc DoFunc) (responseBody []byte, err error) {
+	response, doFuncErr := doFunc(c.Client)
+	defer func() {
+		if response != nil {
+			response.Body.Close()
+		}
+	}()
+
+	if response != nil {
+		var readAllErr error
+		responseBody, readAllErr = io.ReadAll(response.Body)
+		if readAllErr != nil {
+			err = readAllErr
+			return nil, err
+		}
+	}
+
+	if doFuncErr != nil {
+		err = doFuncErr
+		return
+	}
+
+	if response.StatusCode != 200 {
+		err = fmt.Errorf("status code of http response is %d instead of 200", response.StatusCode)
+		return
+	}
+
+	return
+}
+
+func (c *Client) doDump(doFunc DoFunc) (responseBody []byte, dumpData *DumpData, err error) {
+	response, doFuncErr := doFunc(c.Client)
+	dumpData = c.dump(response)
+	responseBody = []byte(dumpData.ResponseBody)
+
+	if doFuncErr != nil {
+		err = doFuncErr
+		return
+	}
+
+	if response.StatusCode != 200 {
+		err = fmt.Errorf("status code of http response is %d instead of 200", response.StatusCode)
+		return
+	}
+
+	return
+}
+
+func (c *Client) dump(response *http.Response) *DumpData {
+	dumpData := new(DumpData)
+
+	if response == nil {
+		return dumpData
+	}
+
+	request := response.Request
+
+	// Dump request
+	dumpRequest, err := httputil.DumpRequest(request, false)
+	dumpData.Request = string(dumpRequest)
+	dumpData.DumpRequestError = err.Error()
+
+	// Dump request body
+	requestBody, err := request.GetBody()
+	defer func() {
+		if requestBody != nil {
+			requestBody.Close()
+		}
+	}()
+	if err != nil {
+		dumpData.DumpRequestBodyError = err.Error()
+	} else {
+		requestBodyBytes, err := io.ReadAll(requestBody)
+		dumpData.RequestBody = string(requestBodyBytes)
+		dumpData.DumpRequestBodyError = err.Error()
+	}
+
+	// Dump response
+	dumpResponse, err := httputil.DumpResponse(response, false)
+	dumpData.Response = string(dumpResponse)
+	dumpData.DumpResponseError = err.Error()
+
+	// Dump response body
+	responseBody := response.Body
+	defer func() {
+		if responseBody != nil {
+			responseBody.Close()
+		}
+	}()
+	responseBodyBytes, err := io.ReadAll(responseBody)
+	dumpData.ResponseBody = string(responseBodyBytes)
+	dumpData.DumpResponseBodyError = err.Error()
+
+	return dumpData
 }
