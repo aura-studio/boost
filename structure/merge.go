d30b03d1ce7bfacc19aead7f613b38c0cebda0bd
tony <lzongti@gmail.com>
2024-06-25 17:52:07 +0800

    add structure

--- structure/merge.go
+++ structure/merge.go
@@ -1,51 +1,19 @@
-package mjson
+package structure
 
-import (
-	"encoding/json"
-)
-
-// Merge merges two deep nested JSON objects and returns the merged result as a byte slice.
-func Merge(jsonData1, jsonData2 []byte) ([]byte, error) {
-	// Parse the first JSON data into a map structure
-	var map1 map[string]interface{}
-	err := json.Unmarshal(jsonData1, &map1)
-	if err != nil {
-		return nil, err
-	}
-
-	// Parse the second JSON data into a map structure
-	var map2 map[string]interface{}
-	err = json.Unmarshal(jsonData2, &map2)
-	if err != nil {
-		return nil, err
-	}
-
-	// Merge the two map structures
-	mergedMap := mergeMaps(map1, map2)
-
-	// Convert the merged map structure back to JSON
-	mergedJSON, err := json.Marshal(mergedMap)
-	if err != nil {
-		return nil, err
-	}
-
-	return mergedJSON, nil
-}
-
-// mergeMaps merges two map structures recursively
-func mergeMaps(map1, map2 map[string]interface{}) map[string]interface{} {
-	mergedMap := make(map[string]interface{})
+// mergeStructure merges two map structures recursively
+func mergeStructure(m1, m2 Structure) Structure {
+	mergedMap := make(Structure)
 
 	// Iterate over the keys of the first map structure
 	// and copy the key-value pairs to the merged map
-	for key, value := range map1 {
+	for key, value := range m1 {
 		mergedMap[key] = value
 	}
 
 	// Iterate over the keys of the second map structure
 	// and copy the key-value pairs to the merged map.
 	// If a key already exists in the merged map, recursively merge the values.
-	for key, value := range map2 {
+	for key, value := range m2 {
 		if existingValue, ok := mergedMap[key]; ok {
 			// If the key already exists, recursively merge the values
 			mergedMap[key] = mergeValues(existingValue, value)
@@ -61,10 +29,10 @@ func mergeMaps(map1, map2 map[string]interface{}) map[string]interface{} {
 // mergeValues merges two values recursively
 func mergeValues(value1, value2 interface{}) interface{} {
 	switch value1 := value1.(type) {
-	case map[string]interface{}:
+	case Structure:
 		// If the value is a map structure, recursively merge the two map structures
-		if value2, ok := value2.(map[string]interface{}); ok {
-			return mergeMaps(value1, value2)
+		if value2, ok := value2.(Structure); ok {
+			return mergeStructure(value1, value2)
 		}
 	case []interface{}:
 		// If the value is a slice, merge the two slices
