4e86017bcf64f4f10b08bfc3f802285daa8739cd
lzongti@gmail.com <lzongti@gmail.com>
2026-01-09 17:56:29 +0800

    Remove redowl documentation and types definition files to streamline the repository structure.

--- redowl/errors.go
+++
@@ -1,9 +0,0 @@
-package redowl
-
-import "errors"
-
-var (
-	ErrInvalidQueueName      = errors.New("redowl: invalid queue name")
-	ErrInvalidReceiptHandle  = errors.New("redowl: invalid receipt handle")
-	ErrTriggersNotConfigured = errors.New("redowl: triggers not configured")
-)
